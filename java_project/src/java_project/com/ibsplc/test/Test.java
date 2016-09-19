61eaacef0c98c122160e2478e10d7af6a4f89afe
Sari <Sari@Sari-Vivek>
2016-09-19 22:27:29 +1000

    Testing

---
+++ java_project/src/java_project/com/ibsplc/test/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package java_project.com.ibsplc.test;
+
+/**
+ * @author Sari
+ *
+ */
+public class Test {
+
+	/**
+	 * 
+	 */
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
